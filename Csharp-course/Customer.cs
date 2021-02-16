288d2700c55f5f57afe08dff6ad824fa811e9d12
Matheus Viana <matheus_o_viana@hotmail.com>
2021-02-16 14:16:30 -0300

    concepts of constructors in c#

---
+++ Csharp-course/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Csharp_course
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int id) 
+            : this() => Id = id;
+
+        public Customer(int id, string name)
+            : this(id) => Name = name;
+    }
+}
---
+++ Csharp-course/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Csharp_course
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "John");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
